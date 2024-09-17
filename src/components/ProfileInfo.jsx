28475f6c52c487921d6e6464c9c6a463a6a90984
Santiago Gamond <ssquarkss@gmail.com>
2024-09-16 23:42:06 -0300

    profile image changes - Profileinfo component beginning

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,7 +1,12 @@
-export default function ProfileInfo() {
+export default function ProfileInfo({username, description}) {
     return (
-        <div>
-            
+        <div className="Profile__info-container">
+            <div className="Profile__info-container__name-container">
+                <h1>{username}</h1>
+            </div>
+            <div className="Profile__info-container__description-container">
+                {description}
+            </div>
         </div>
     )
 }
